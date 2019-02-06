d47f92842fa432fe55820af9af54676974c48973
Johan van Rhyn <github@jvanrhyn.co.za>
2019-02-06 23:28:37 +0200

    Added pages to try out templates

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ import re
 
 app = Flask(__name__)
 
+# Replace the existing home function with the one below
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
+
+# New functions
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/hello/<name>")
 def hello_there(name = None):
